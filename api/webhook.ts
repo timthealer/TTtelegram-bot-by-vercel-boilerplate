42576748643db7e4ce25c829a36a1c1f6388b6dd
timthealer <timthealer@gmail.com>
2026-07-04 21:34:43 +0300

    Update webhook.ts

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,9 +1,9 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import bot from "../src/bot";
+import bot from "../src";
 
 export default async function handler(
-    req: VercelRequest,
-    res: VercelResponse
+  req: VercelRequest,
+  res: VercelResponse
 ) {
-    return bot.webhookCallback("/api/webhook")(req, res);
+  return bot.webhookCallback("/api/webhook")(req, res);
 }
